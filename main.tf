319cfbaa54f7d5165d04bc4e77ad2349879c1977
Ludovico Magnocavallo <ludomagno@google.com>
2019-09-20 14:48:35 +0200

    Change router variable to optional, and create it internally if not passed in

--- main.tf
+++ main.tf
@@ -25,27 +25,34 @@ locals {
   default_name                   = "cloud-nat-${random_string.name_suffix.result}"
   nat_ips_length                 = length(var.nat_ips)
   default_nat_ip_allocate_option = local.nat_ips_length > 0 ? "MANUAL_ONLY" : "AUTO_ONLY"
-
   # locals for google_compute_router_nat
   nat_ip_allocate_option = var.nat_ip_allocate_option ? var.nat_ip_allocate_option : local.default_nat_ip_allocate_option
   name                   = var.name != "" ? var.name : local.default_name
+  router                 = var.router == "" ? google_compute_router.router[0].name : var.router
+}
+
+resource "google_compute_router" "router" {
+  count   = var.router == "" ? 1 : 0
+  name    = "${local.name}-router"
+  project = var.project_id
+  region  = var.region
+  network = var.network
+  bgp {
+    asn = var.router_asn
+  }
 }
 
 resource "google_compute_router_nat" "main" {
-  project = var.project_id
-  region  = var.region
-
-  name   = local.name
-  router = var.router
-
+  project                            = var.project_id
+  region                             = var.region
+  name                               = local.name
+  router                             = local.router
   nat_ip_allocate_option             = local.nat_ip_allocate_option
   nat_ips                            = var.nat_ips
   source_subnetwork_ip_ranges_to_nat = var.source_subnetwork_ip_ranges_to_nat
-
-  min_ports_per_vm                 = var.min_ports_per_vm
-  udp_idle_timeout_sec             = var.udp_idle_timeout_sec
-  icmp_idle_timeout_sec            = var.icmp_idle_timeout_sec
-  tcp_established_idle_timeout_sec = var.tcp_established_idle_timeout_sec
-  tcp_transitory_idle_timeout_sec  = var.tcp_transitory_idle_timeout_sec
+  min_ports_per_vm                   = var.min_ports_per_vm
+  udp_idle_timeout_sec               = var.udp_idle_timeout_sec
+  icmp_idle_timeout_sec              = var.icmp_idle_timeout_sec
+  tcp_established_idle_timeout_sec   = var.tcp_established_idle_timeout_sec
+  tcp_transitory_idle_timeout_sec    = var.tcp_transitory_idle_timeout_sec
 }
-
