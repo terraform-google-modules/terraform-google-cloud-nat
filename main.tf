62bb5a6fb8dda5e301ea629a13743f213949e188
LeandroFPeixoto <141767188+LeandroFPeixoto@users.noreply.github.com>
2024-08-20 10:29:34 -0300

    feat: add drain_nat_ips support

--- main.tf
+++ main.tf
@@ -55,6 +55,7 @@ resource "google_compute_router_nat" "main" {
   router                              = local.router
   nat_ip_allocate_option              = local.nat_ip_allocate_option
   nat_ips                             = var.nat_ips
+  drain_nat_ips                       = var.drain_nat_ips
   source_subnetwork_ip_ranges_to_nat  = var.source_subnetwork_ip_ranges_to_nat
   min_ports_per_vm                    = var.min_ports_per_vm
   max_ports_per_vm                    = var.enable_dynamic_port_allocation ? var.max_ports_per_vm : null
