e4f136aca45d8bc90bc307a3749913f17c9e1933
Hossam Mostafa <hossamdash2@gmail.com>
2025-11-04 16:35:54 +0200

    feat: update min_ports_per_vm logic and default value to be aligned with provider recommendations
    https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_router_nat#min_ports_per_vm-1

--- variables.tf
+++ variables.tf
@@ -33,7 +33,7 @@ variable "icmp_idle_timeout_sec" {
 variable "min_ports_per_vm" {
   type        = string
   description = "Minimum number of ports allocated to a VM from this NAT config. Defaults to 64 if not set. Changing this forces a new NAT to be created."
-  default     = "64"
+  default     = null
 }
 
 variable "max_ports_per_vm" {
